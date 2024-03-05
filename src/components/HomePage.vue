2cda72adddc09c7a2ec97a8b436297920570c8f0
Susan56789 <devnimoh@gmail.com>
2024-03-05 13:12:27 +0300

    iNITIAL cOMMIT

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        Home Page
+        <div class="car-movement">
+            <img src="https://images.unsplash.com/photo-1618846446712-a4eda2adc05f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGxhbWJvcmdoaW5pfGVufDB8fDB8fHww"
+                alt="Car" class="car" />
+        </div>
     </div>
 </template>
 
@@ -9,3 +12,42 @@ export default {
     name: 'HomePage'
 }
 </script>
+
+<style scoped>
+.car-movement {
+    overflow: hidden;
+    font-size: 40px;
+    height: 50px;
+}
+
+.car-movement>.car {
+    position: relative;
+    display: inline-block;
+    animation: linear infinite;
+    animation-name: run;
+    animation-duration: 5s;
+}
+
+
+@keyframes run {
+    0% {
+        transform: rotateY(180deg);
+        left: -100px;
+    }
+
+    50% {
+        transform: rotateY(180deg);
+        left: 100%;
+    }
+
+    51% {
+        transform: rotateY(0deg);
+        left: calc(100% + 100px);
+    }
+
+    100% {
+        transform: rotateY(0deg);
+        left: -100px;
+    }
+}
+</style>
