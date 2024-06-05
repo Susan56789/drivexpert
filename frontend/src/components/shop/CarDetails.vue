10429ae9d85a447d439e797727e6f69a7ddef1b5
Susan56789 <devnimoh@gmail.com>
2024-06-05 16:56:51 +0300

    styling

--- frontend/src/components/shop/CarDetails.vue
+++ frontend/src/components/shop/CarDetails.vue
@@ -2,18 +2,134 @@
     <div class="container mx-auto px-4 py-8">
         <div v-if="car">
             <h1 class="text-3xl font-bold mb-4">{{ car.carName }}</h1>
-            <div v-if="car.images && car.images.length" class="flex overflow-x-scroll space-x-4 mb-4">
-                <img crossorigin="anonymous" :src=car.images[0].url :alt="car.carName"
-                    class="w-full h-48 object-cover rounded-md" />
+            <div v-if="car.images && car.images.length" class="flex space-x-4 mb-4">
+                <carousel :items-to-show="1.5">
+                    <slide v-for="image in car.images" :key="image.url">
+                        <img crossorigin="anonymous" :src=image.url :alt="car.carName"
+                            class="w-full h-400 object-cover rounded-md" />
+                    </slide>
+
+                    <template #addons>
+                        <navigation />
+                        <pagination />
+                    </template>
+                </carousel>
+
             </div>
-            <p><strong>Price:</strong> {{ formatCurrency(car.price) }}</p>
-            <p><strong>Year:</strong> {{ car.year }}</p>
-            <p><strong>Fuel Type:</strong> {{ car.fuelType }}</p>
-            <p><strong>Engine Size:</strong> {{ car.engineSize }}</p>
-            <p><strong>Mileage:</strong> {{ car.mileage }}</p>
-            <p><strong>Location:</strong> {{ car.currentLocation }}</p>
             <p class="mt-4">{{ car.description }}</p>
+            <div class="mx-auto flex max-w-4xl  gap-4  p-1">
+                <div class="col-span-2 rounded-lg p-32 ">
+                    <!-- Main Content -->
+                    <div class="flex flex-col">
+
+                        <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+                            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="overflow-hidden">
+                                    <table class="min-w-full">
+                                        <tbody>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Ad Created
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ dateTime(date, car.createdAt) }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Price
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ formatCurrency(car.price) }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Year
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ car.year }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Fuel Type
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ car.fuelType }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Engine Size
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ car.engineSize }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Mileage
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ car.mileage }}
+                                                </td>
+                                            </tr>
+                                            <tr class="bg-gray-100 border-b">
+
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    Location
+                                                </td>
+                                                <td
+                                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                    {{ car.currentLocation }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-span-12 rounded-lg  p-16 sm:col-span-4">
+                    <!-- Sidebar -->
+                    <section class="flex place-items-center bg-red-300 p-16">
+                        <div
+                            class="flex max-w-xs items-center border-l-8 border-red-500 bg-red-50 p-4 text-white-900 shadow-lg">
+                            <div class="min-w-0">
+                                <h2 class="overflow-hidden text-ellipsis whitespace-nowrap">Seller Details</h2>
+                                <p>{{ car.seller.name }}</p>
+                                <p>{{ car.seller.email }}</p>
+                                <p>{{ car.seller.phone }}</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
         </div>
+
+
         <div v-else>
             <p>Loading...</p>
         </div>
@@ -22,9 +138,18 @@
 
 <script>
 import axios from 'axios';
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import moment from 'moment';
 
 export default {
     name: 'CarDetails',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
     props: {
         id: {
             type: String,
@@ -55,6 +180,9 @@ export default {
         formatCurrency(value) {
             const numericValue = parseFloat(value);
             return isNaN(numericValue) ? '-' : numericValue.toLocaleString('en-KE', { style: 'currency', currency: 'KES' });
+        },
+        dateTime(value) {
+            return moment(value).format('YYYY-MM-DD');
         },
     }
 };
